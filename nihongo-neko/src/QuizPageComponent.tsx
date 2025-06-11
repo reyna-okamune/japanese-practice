0c72e800d3d1a328f0882517b26891ed777ddcfe
rokamune <reynaoaguirre@gmail.com>
2025-06-11 14:16:11 -0700

    Connecting Quiz Page to Main Component and Populating Section 0 Data from Tutoring Lesson

---
+++ nihongo-neko/src/QuizPageComponent.tsx
@@ -0,0 +1,16 @@
+import { tutorSection0Data } from './data/tutoringData/TutorSection0Data'
+
+function QuizPageComponent() {
+
+    const mockData = tutorSection0Data
+    console.log(mockData)
+
+
+    return (
+    <>
+      <p>Testing Quiz Page</p>
+    </>
+  )
+}
+
+export default QuizPageComponent
---
+++ nihongo-neko/src/data/tutoringData/TutorSection0Data.ts
@@ -0,0 +1,181 @@
+export interface TutorQuizQuestion {
+    id: number;
+    section: number;
+    prompt: string;
+    question: string;
+    answer: string;
+}
+
+export const tutorSection0Data: TutorQuizQuestion[] = [
+    {
+        id: 1,
+        section: 0,
+        question: "Translate the sentence in Japanese",
+        prompt: "I am Mike.",
+        answer: "私はマイクです。"
+
+    },
+    {
+        id: 2,
+        section: 0,
+        question: "Translate the sentence in Japanese",
+        prompt: "I am a student.",
+        answer: "私は日本人です。"
+    },
+    {
+        id: 3,
+        section: 0,
+        question: "Translate the sentence in Japanese (polite form)",
+        prompt: "I am not American.",
+        answer: "私はアメリカ人じゃありません。"
+    }, 
+    {
+        id: 4,
+        section: 0,
+        question: "Turn the sentence to the negative form (polite form)",
+        prompt: "私は生徒（せいと）です。",
+        answer: "私は生徒じゃありません。"
+    },
+    {
+        id: 5,
+        section: 0,
+        question: "Turn the sentence to the negative form (polite form)",
+        prompt: "私は先生（せんせい）です。",
+        answer: "私は先生（せんせい）じゃありません。"
+    },
+    {
+        id: 6,
+        section: 0,
+        question: "Turn the sentence to the negative form (polite form)",
+        prompt: "私はマイクです。",
+        answer: "私はマイクじゃありません。"
+    },
+    {
+        id: 7,
+        section: 0,
+        question: "Translate the sentence in Japanese (super polite form)",
+        prompt: "I am not American.",
+        answer: "私はアメリカ人ではありません。"
+    },
+    {
+        id: 8,
+        section: 0,
+        question: "Turn the sentence to the negative form (super polite form)",
+        prompt: "私はマイクです。",
+        answer: "私はマイクではありません。"
+    },
+    {
+        id: 9,
+        section: 0,
+        question: "Turn the sentence to the negative form (super polite form)",
+        prompt: "私は生徒（せいと）です。",
+        answer: "私は生徒ではありません。"
+    },
+    {
+        id: 10,
+        section: 0,
+        question: "Turn the sentence to the negative form (super polite form)",
+        prompt: "私は先生（せんせい）です。",
+        answer: "私は先生ではありません。"
+    },
+    {
+        id: 11,
+        section: 0,
+        question: "Turn the sentence to the negative form (super polite form)",
+        prompt: "私は日本人（にほんじん）です。",
+        answer: "私は日本人ではありません。"
+    }, 
+    {
+        id: 12,
+        section: 0,
+        question: "Translate the sentence in Japanese",
+        prompt: "Is Mr.Miller an office worker?",
+        answer: "ミラーさんは会社員（かいしゃいん）ですか。"
+    },
+    {
+        id: 13,
+        section: 0,
+        question: "Translate the sentence in Japanese",
+        prompt: "Is this apple sweet?",
+        answer: "このりんごはあまいですか？"
+    },
+    {
+        id: 14,
+        section: 0,
+        question: "Translate the sentence in Japanese",
+        prompt: "Are you American?",
+        answer: "あなたはアメリカ人ですか？"
+    },
+    {
+        id: 15,
+        section: 0,
+        question: "Translate the sentence in Japanese",
+        prompt: "Is she a teacher?",
+        answer: "あのかのじょは先生ですか？"
+    },
+    {
+        id: 16,
+        section: 0,
+        question: "Translate the sentence in Japanese",
+        prompt: "Aren't you American? No, I'm not./Yes, I'm, American.",
+        answer: "あなたはアメリカ人じゃありませんか？はい、ちがいます。/いいえ、アメリカ人です。"
+    },
+    {
+        id: 17,
+        section: 0,
+        question: "Translate this vocabulary word",
+        prompt: "girlfriend/she",
+        answer: "かのじょ"
+    },
+    {
+        id: 18,
+        section: 0,
+        question: "Translate this vocabulary word",
+        prompt: "boyfriend",
+        answer: "かれし"
+    },
+    {
+        id: 19,
+        section: 0,
+        question: "Translate this vocabulary word",
+        prompt: "he",
+        answer: "かれ"
+    },
+    {
+        id: 20,
+        section: 0,
+        question: "Translate this vocabulary word",
+        prompt: "last month",
+        answer: "せんげつ"
+    },
+    {
+        id: 21,
+        section: 0,
+        question: "Translate this vocabulary word",
+        prompt: "popular",
+        answer: "にんきな"
+    },
+    {
+        id: 22,
+        section: 0,
+        question: "Translate this vocabulary word",
+        prompt: "student (not as occupation)",
+        answer: "せいと"
+    },
+    {
+        id: 23,
+        section: 0,
+        question: "Translate this vocabulary word",
+        prompt: "is not/am not/are not (polite)",
+        answer: "じゃありません"
+    },
+    {
+        id: 24,
+        section: 0,
+        question: "Translate this vocabulary word",
+        prompt: "is not/am not/are not (super polite)",
+        answer: "ではありません"
+    },
+    
+    
+]
